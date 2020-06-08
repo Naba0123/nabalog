e6726c20cf1d927222ec4dd255d65b7dfcb2081c
Naba <11944066+Naba0123@users.noreply.github.com>
2020-06-08 22:49:38 +0900

    google add

--- src/html.js
+++ src/html.js
@@ -12,12 +12,12 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
-      </head>
-      <script
+        <script
         data-ad-client="ca-pub-3277188906130312"
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
       ></script>
+      </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
         <div
