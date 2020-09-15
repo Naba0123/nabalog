4b99123a0bd1b424cdb1fd7ea8f6b5c7d8c8b8db
Naba <11944066+Naba0123@users.noreply.github.com>
2020-09-15 22:41:06 +0900

    chapter 4

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,6 +5,11 @@ import Footer from "../components/footer"
 
 import "./layout.css"
 
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import {config} from "@fortawesome/fontawesome-svg-core"
+
+config.autoAddCss = false
+
 export default ({ children }) => (
   <div>
     <Header />
